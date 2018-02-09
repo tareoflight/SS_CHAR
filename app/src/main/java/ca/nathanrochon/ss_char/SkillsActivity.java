7701dadd94beed123af621254502b43bc455bb2d
tareoflight <nathan.rochon@gmail.com>
2018-02-09 06:51:56 -0500

    layoutsAlpha
    Started the skill layout

--- app/src/main/java/ca/nathanrochon/ss_char/SkillsActivity.java
+++ app/src/main/java/ca/nathanrochon/ss_char/SkillsActivity.java
@@ -3,16 +3,11 @@ package ca.nathanrochon.ss_char;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SkillsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public void onClick(view v){
-
-
-
+        setContentView(R.layout.activity_skills);
     }
 }
