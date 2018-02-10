23953fafed75ab7a2c5ee6eb8e4207b9d8582d8a
tareoflight <nathan.rochon@gmail.com>
2018-02-10 14:30:52 -0500

    added skills ativity

--- app/src/main/java/ca/nathanrochon/ss_char/MainActivity.java
+++ app/src/main/java/ca/nathanrochon/ss_char/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.nathanrochon.ss_char;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void skillTrans(View view){
+        startActivity(new Intent(MainActivity.this, SkillsActivity.class));
     }
 }
