6cdcdf11810a3e53e8335b76055a637a82c635af
tareoflight <nathan.rochon@gmail.com>
2018-02-12 12:06:46 -0500

    Revert "Merge branch 'Layouts' of https://github.com/tareoflight/SS_Char into Layouts"
    This reverts commit b6cd127580c3cfebfd4a66a40f70deb8c1e19c36, reversing
    changes made to 6e17552197679b0a8eb07b10490ec57ce2ce72a2.

--- app/src/main/java/ca/nathanrochon/ss_char/MainActivity.java
+++ app/src/main/java/ca/nathanrochon/ss_char/MainActivity.java
@@ -1,7 +1,9 @@
 package ca.nathanrochon.ss_char;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onClick(view v){
-
-
-
+    public void skillTrans(View view){
+        startActivity(new Intent(MainActivity.this, SkillsActivity.class));
     }
 }
