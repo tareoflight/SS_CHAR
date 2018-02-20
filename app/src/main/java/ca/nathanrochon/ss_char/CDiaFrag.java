4aa3db875dbe4c43e7c29ef3803d025e39a7efe5
tareoflight <nathan.rochon@gmail.com>
2018-02-20 14:51:16 -0500

    saveing for skills
    added saveing aloading of skill valuses and subvaluses

--- app/src/main/java/ca/nathanrochon/ss_char/CDiaFrag.java
+++ app/src/main/java/ca/nathanrochon/ss_char/CDiaFrag.java
@@ -1,36 +1,97 @@
 package ca.nathanrochon.ss_char;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CDiaFrag extends DialogFragment {
 
+    String name;
+    View clicker;
+    int total;
+
     @Override
     public Dialog onCreateDialog(Bundle bundle){
+
+
+
         AlertDialog.Builder B = new AlertDialog.Builder(getActivity());
-        LayoutInflater F = getActivity().getLayoutInflater();
-        B.setView(F.inflate(R.layout.skill_popup,null))
+        final LayoutInflater F = getActivity().getLayoutInflater();
+        final View inlat = F.inflate(R.layout.skill_popup,null);
+        final SharedPreferences data = getActivity().getSharedPreferences("ss_char."+name, 0);
+
+        ((EditText)inlat.findViewById(R.id.skillRank)).setText(data.getInt("Rank",2)+"");
+        ((EditText)inlat.findViewById(R.id.skillClassB)).setText(data.getInt("ClassB",1)+"");
+        ((EditText)inlat.findViewById(R.id.skillABIL)).setText(data.getInt("ABIL",2)+"");
+        ((EditText)inlat.findViewById(R.id.skillRace)).setText(data.getInt("Race",2)+"");
+        ((EditText)inlat.findViewById(R.id.skillFeat)).setText(data.getInt("Feat",3)+"");
+        ((EditText)inlat.findViewById(R.id.skillItem)).setText(data.getInt("Item",4)+"");
+        ((EditText)inlat.findViewById(R.id.skillMisc)).setText(data.getInt("Misc",5)+"");
+        ((EditText)inlat.findViewById(R.id.skillArmor)).setText(data.getInt("Armor",6)+"");
+        final TextView totaler= (TextView) clicker;
+                B.setView(inlat)
                 .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.v("skill popup: ","Save");
+                        int tt;
+                        SharedPreferences.Editor saver = data.edit();
+                        tt=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillRank)).getText().toString());
+                        saver.putInt("Rank",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillRank)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillClassB)).getText().toString());
+                        saver.putInt("ClassB",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillClassB)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillABIL)).getText().toString());
+                        saver.putInt("ABIL",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillABIL)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillRace)).getText().toString());
+                        saver.putInt("Race",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillRace)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillFeat)).getText().toString());
+                        saver.putInt("Feat",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillFeat)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillItem)).getText().toString());
+                        saver.putInt("Item",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillItem)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillMisc)).getText().toString());
+                        saver.putInt("Misc",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillMisc)).getText().toString()));
+                        tt+=Integer.parseInt(((EditText)inlat.findViewById(R.id.skillArmor)).getText().toString());
+                        saver.putInt("Armor",Integer.parseInt(((EditText)inlat.findViewById(R.id.skillArmor)).getText().toString()));
+                        saver.putInt("Total",tt);
+                        saver.commit();
+                        total=tt;
+                        totaler.setText(total+"");
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.v("skill popup: ","Cancel");
+                        Log.i("skill"+name+" popup: ","Cancel");
 
                     }
                 });
-        return B.create();
+
+
+
+        B.setTitle(name);
+        AlertDialog output = B.create();
+
+
+        return output;
 
 
     }
 
+    public void setName(String name){
+        this.name = name;
+
+    }
+
+    public void setClicker(View v) {
+        clicker = v;
+    }
 }
--- app/src/main/java/ca/nathanrochon/ss_char/SkillsActivity.java
+++ app/src/main/java/ca/nathanrochon/ss_char/SkillsActivity.java
@@ -1,14 +1,9 @@
 package ca.nathanrochon.ss_char;
 
-import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
@@ -18,13 +13,78 @@ public class SkillsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skills);
+
+        load(R.id.SKT1);
+        load(R.id.SKT2);
+        load(R.id.SKT3);
+        load(R.id.SKT4);
+        load(R.id.SKT5);
+        load(R.id.SKT6);
+        load(R.id.SKT7);
+        load(R.id.SKT8);
+        load(R.id.SKT9);
+        load(R.id.SKT10);
+        load(R.id.SKT11);
+        load(R.id.SKT12);
+        load(R.id.SKT13);
+        load(R.id.SKT14);
+        load(R.id.SKT15);
+        load(R.id.SKT16);
+        load(R.id.SKT17);
+        load(R.id.SKT18);
+        load(R.id.SKT19);
+        load(R.id.SKT20);
+        load(R.id.SKT21);
+        load(R.id.SKT22);
+        load(R.id.SKT23);
+        load(R.id.SKT24);
+        load(R.id.SKT25);
+        load(R.id.SKT26);
+        load(R.id.SKT27);
+        load(R.id.SKT28);
+        load(R.id.SKT29);
+        load(R.id.SKT30);
+        load(R.id.SKT31);
+        load(R.id.SKT32);
+        load(R.id.SKT33);
+        load(R.id.SKT34);
+        load(R.id.SKT35);
+        load(R.id.SKT36);
+        load(R.id.SKT37);
+        load(R.id.SKT38);
+        load(R.id.SKT39);
+
+
+
+
+
+
+
+
+
     }
 
 
     public void onSkillClick(View v){
         DialogFragment nF = new CDiaFrag();
         String name =((TextView)v).getText().toString();
+
+
+
+        ((CDiaFrag)nF).setName(name);
+        ((CDiaFrag)nF).setClicker(findViewById(v.getLabelFor()));
         nF.show(getFragmentManager(),"yep");
+
+    }
+    public void setLableTotal(View v, int total){
+        TextView child = (TextView) findViewById(v.getLabelFor());
+        child.setText(total+"");
+    }
+
+    private void load(int ParentID){
+       TextView v = findViewById(findViewById(ParentID).getLabelFor());
+        SharedPreferences data = getSharedPreferences("ss_char."+((TextView)findViewById(ParentID)).getText().toString(), 0);
+        v.setText(data.getInt("Total",99)+"");
 
     }
 }
